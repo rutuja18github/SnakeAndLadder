8cf27f8effed8431c66d970359800d6ae75ae81d
RutujaPatil18 <prutuja189@gmail.com>
2022-09-19 01:30:14 +0530

    Add:Rolls die to get number

--- src/main/java/com/snake/ladder/App.java
+++ src/main/java/com/snake/ladder/App.java
@@ -11,8 +11,9 @@ public class App {
 	private static final Logger logger = LogManager.getLogger(App.class);
 
 	public static void main(String[] args) {
-		logger.info( "Welcome to Snake and Ladder Game " );
+		logger.info( "Welcome to Snake and Ladder Game" );
     	Player playObj=new Player();
-    	logger.info("Player's initial position :"+playObj.getPlayerPosition());
+    	logger.info("Player's initial position is :"+playObj.getPlayerPosition());
+    	playObj.diceRoll();
 	}
 }
--- src/main/java/com/snake/ladder/Player.java
+++ src/main/java/com/snake/ladder/Player.java
@@ -1,12 +1,21 @@
 package com.snake.ladder;
 
+import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class Player {
 
+	private static final Logger logger = LogManager.getLogger(Player.class);
 	private int playerPosition;
+	int dieValue;
 
 	Player() {
 		playerPosition = 0;
 	}
+
+	Random random = new Random();
 
 	public int getPlayerPosition() {
 		return playerPosition;
@@ -14,6 +23,11 @@ public class Player {
 
 	public void setPlayerPosition(int playerPosition) {
 		this.playerPosition = playerPosition;
+	}
+
+	public void diceRoll() {
+		dieValue = random.nextInt(6) + 1;
+		logger.info("After rolling die get value on die :" + dieValue);
 	}
 
 }
