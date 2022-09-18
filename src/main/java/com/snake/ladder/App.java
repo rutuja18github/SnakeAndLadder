b3439972b1aa778cb59228942c3c8f912a9cd0bb
RutujaPatil18 <prutuja189@gmail.com>
2022-09-19 01:16:31 +0530

    Add:Initial commit

---
+++ src/main/java/com/snake/ladder/App.java
@@ -0,0 +1,16 @@
+package com.snake.ladder;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private static final Logger logger = LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("Welcome to Snake and Ladder Game");
+	}
+}
